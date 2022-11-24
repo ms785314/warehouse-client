ea9098168fabe4b65b2028dc225a9182e6193538
ms785314 <ms785314@gmail.com>
2022-11-24 10:00:26 +0600

    changed in index js file

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
-      <HashRouter>
+      
         <App />
-      </HashRouter>
+      
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 
 );
 
